846568ef97401cbf4801e3cc0e3e25428236b47c
szeist <szeist@gmail.com>
2015-11-12 18:29:49 +0100

    fix(error-handler-middleware): don't yield render

--- app/middlewares/error-handler.js
+++ app/middlewares/error-handler.js
@@ -2,11 +2,11 @@
 
 var logger = require('logentries-logformat')('app');
 
-var renderError = function* (context, message) {
+var renderError = function(context, message) {
   if (context.is('application/json')) {
     context.body = { error: true, message: message };
   } else {
-    yield context.render('main/error', { message: message });
+    context.render('main/error', { message: message });
   }
 };
 
@@ -16,11 +16,11 @@ module.exports = function* (next) {
   } catch (ex) {
     if (ex.code === 401) {
       this.status = 401;
-      yield renderError(this, 'We\'re sorry, but You are not authorized to take this action');
+      renderError(this, 'We\'re sorry, but You are not authorized to take this action');
       logger.error('authentication', ex.message);
     } else {
       this.status = 500;
-      yield renderError(this, 'We\'re sorry, but something went wrong');
+      renderError(this, 'We\'re sorry, but something went wrong');
       logger.error('internal', ex.message, { stack: ex.stack });
     }
   }
