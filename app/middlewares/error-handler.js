19098aa7ef2e01c20bcd0e92c1ef64921240876d
szeist <szeist@gmail.com>
2015-11-13 16:50:46 +0100

    feat(error-handler-middleware): render specified error page or set body to the error message

--- app/middlewares/error-handler.js
+++ app/middlewares/error-handler.js
@@ -2,26 +2,34 @@
 
 var logger = require('logentries-logformat')('app');
 
+var errorPagePath;
+
 var renderError = function(context, message) {
   if (context.is('application/json')) {
     context.body = { error: true, message: message };
+  } else if (errorPagePath) {
+    context.render(errorPagePath, { message: message });
   } else {
-    context.render('main/error', { message: message });
+    context.body = message;
   }
 };
 
-module.exports = function* (next) {
-  try {
-    yield next;
-  } catch (ex) {
-    if (ex.code === 401) {
-      this.status = 401;
-      renderError(this, 'We\'re sorry, but You are not authorized to take this action');
-      logger.error('authentication', ex.message);
-    } else {
-      this.status = 500;
-      renderError(this, 'We\'re sorry, but something went wrong');
-      logger.error('internal', ex.message, { stack: ex.stack });
+module.exports = function(errorPage) {
+  errorPagePath = errorPage;
+
+  return function* (next) {
+    try {
+      yield next;
+    } catch (ex) {
+      if (ex.code === 401) {
+        this.status = 401;
+        renderError(this, 'We\'re sorry, but You are not authorized to take this action');
+        logger.error('authentication', ex.message);
+      } else {
+        this.status = 500;
+        renderError(this, 'We\'re sorry, but something went wrong');
+        logger.error('internal', ex.message, { stack: ex.stack });
+      }
     }
   }
 };
---
+++ app/middlewares/error-handler.spec.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var expect = require('chai').expect;
+var errorHandler = require('./error-handler');
+
+describe('Error Handler Middleware', function() {
+
+  const DEFAULT_ERROR_MESSAGE = 'We\'re sorry, but something went wrong';
+  const ERROR_PAGE_PATH = 'path/to/error/page';
+
+  let fakeContext;
+  let nextError = function* () { throw new Error() };
+
+  beforeEach(function() {
+    fakeContext = {
+      renderArgs: {
+        path: null,
+        data: null
+      },
+
+      body: '',
+
+      is: function() {
+        return false;
+      },
+
+      render: function(path, data) {
+        this.renderArgs.path = path;
+        this.renderArgs.data = data;
+      }
+    }
+  });
+
+  it('should render error page if specified', function* () {
+    var middleware = errorHandler(ERROR_PAGE_PATH);
+
+    yield middleware.call(fakeContext, nextError);
+
+    expect(fakeContext.renderArgs.path).to.eql(ERROR_PAGE_PATH);
+    expect(fakeContext.renderArgs.data).to.eql({ message: DEFAULT_ERROR_MESSAGE });
+  });
+
+  it('should render error to request body if no error page path specified', function* () {
+    var middleware = errorHandler();
+
+    yield middleware.call(fakeContext, nextError);
+
+    expect(fakeContext.body).to.eql(DEFAULT_ERROR_MESSAGE);
+  });
+
+});
+
