9ee5ceb846d7df427d9d073f9e85fd40e0ab72d3
Gabor Balla <gabor.balla@emarsys.com>
2016-01-29 11:36:13 +0100

    feat(error-handler-middleware): render unauthorized error if error status is 401 too

--- app/middlewares/error-handler.spec.js
+++ app/middlewares/error-handler.spec.js
@@ -6,6 +6,7 @@ var errorHandler = require('./error-handler');
 describe('Error Handler Middleware', function() {
 
   const DEFAULT_ERROR_MESSAGE = 'We\'re sorry, but something went wrong';
+  const UNAUTHORIZED_ERROR_MESSAGE = 'We\'re sorry, but You are not authorized to take this action';
   const ERROR_PAGE_PATH = 'path/to/error/page';
 
   let fakeContext;
@@ -46,6 +47,40 @@ describe('Error Handler Middleware', function() {
     yield middleware.call(fakeContext, nextError);
 
     expect(fakeContext.body).to.eql(DEFAULT_ERROR_MESSAGE);
+  });
+
+  ['code', 'status'].forEach(propertyName => {
+
+    describe(`error ${propertyName} is 401`, function () {
+
+      it('should render unauthorized message to request body if no error page path specified', function* () {
+        var middleware = errorHandler();
+        let unauthorizedError = function* () {
+          let error = new Error();
+          error[propertyName] = 401;
+          throw error;
+        };
+
+        yield middleware.call(fakeContext, unauthorizedError);
+
+        expect(fakeContext.body).to.eql(UNAUTHORIZED_ERROR_MESSAGE);
+      });
+
+      it('should render unauthorized message to error page if specified', function* () {
+        var middleware = errorHandler();
+        let unauthorizedError = function* () {
+          let error = new Error();
+          error[propertyName] = 401;
+          throw error;
+        };
+
+        yield middleware.call(fakeContext, unauthorizedError);
+
+        expect(fakeContext.body).to.eql(UNAUTHORIZED_ERROR_MESSAGE);
+      });
+
+    });
+
   });
 
 });
